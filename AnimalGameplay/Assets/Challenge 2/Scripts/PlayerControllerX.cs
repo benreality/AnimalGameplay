cab978409be89e11e6a02bae374f397eb92b1131
Ben Chan <100125343+benreality@users.noreply.github.com>
2023-07-07 17:06:33 +0800

    Second commit

---
+++ AnimalGameplay/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeSinceLastSpawn;
+    private float spawnCoolDown = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Increment the timer since the last spawn
+        timeSinceLastSpawn += Time.deltaTime;
+
+        // On spacebar press and time since last spawn longer than spawn cool down, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeSinceLastSpawn >= spawnCoolDown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            //Reset the timer
+            timeSinceLastSpawn = 0f;
+
+            
+        }
+    }
+}
