996c62b1f952e097bb1054e82a2eeea9f35d4cb1
Ben Chan <100125343+benreality@users.noreply.github.com>
2023-07-18 10:47:38 +0800

    Second Game Done

--- JumpCity/Assets/Scripts/PlayerController.cs
+++ JumpCity/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,21 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private Animator animator;
+    public ParticleSystem explosion;
+    public ParticleSystem dirt;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Physics.gravity *= gravityModifier;
         playerRb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,10 +34,12 @@ public class PlayerController : MonoBehaviour
 
     void PlayerJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            animator.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -37,11 +48,18 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirt.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacles"))
+
         {
             gameOver = true;
             Debug.Log("Game Over!");
-        }
+            animator.SetBool("Death_b", true);
+            animator.SetInteger("DeathType_int", 1);
+            explosion.Play();
+            dirt.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }   
     }
 }
